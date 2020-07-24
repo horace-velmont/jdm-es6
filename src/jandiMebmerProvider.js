a9ec038a3e37330d5e2a6b27e13206ab23737629
Horace Velmont <dragond7@naver.com>
2020-07-25 02:43:43 +0900

    add last

--- src/jandiMebmerProvider.js
+++ src/jandiMebmerProvider.js
@@ -15,7 +15,20 @@ class JandiMebmerProvider {
             '나부랭이': 'silverthreadk',
             '블록': 'ch-4ml',
             '쏘': 'infiduk',
-            'Halley': 'ycs1m1yk'
+            'Halley': 'ycs1m1yk',
+            '온누': 'UniTaeMin',
+            'KHJ': 'KHJcode',
+            '리나': 'minjijo',
+            '떼껄룩': 'kwonchanmi',
+            'ccpo': 'ccppoo',
+            '복이': 'changbokLee',
+            'yujo': 'YUJO42',
+            'joe': 'porquelaquiero',
+            '하이': 'kys4548',
+            '소니': 'GDana',
+            '병아리': 'cruisediary',
+            '방탕성현단': 'Seonghy',
+            '양념치킨': 'KEJdev'
         };
     }
 }
